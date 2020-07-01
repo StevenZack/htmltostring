a3f9f78d9953821876b1d6007fcc3b8ebb8cdafe
stevenzack <zhouyuhao@xiaoheiban.cn>
2020-07-01 14:07:11 +0800

    dir

--- file.go
+++ file.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -26,14 +27,14 @@ func getFilelist(root string) {
 			return nil
 		}
 		switch filepath.Ext(path) {
-		case ".html", ".js", ".css", "tis", ".svg", ".jpeg", ".png", ".ico", "woff", "woff2", "ttf", "ttc", ".glade":
+		case ".html", ".js", ".css", "tis", ".svg", ".jpeg", ".png", ".ico", "woff", "woff2", "ttf", "ttc", ".glade", ".xml", ".yaml", ".yml":
 		default:
 			return nil
 		}
 		name := strToolkit.ToCamelCase(strings.Replace(strings.Replace(f.Name(), ".", "_", -1), "-", "_", -1))
 		fo, e := os.OpenFile(root+"views/"+name+".go", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 		if e != nil {
-			fmt.Println("fo() failed:", e)
+			log.Println("fo() failed:", e)
 			return nil
 		}
 		defer fo.Close()
@@ -41,7 +42,7 @@ func getFilelist(root string) {
 
 var Bytes_` + name + " =[]byte{")
 		if e != nil {
-			fmt.Println("writeString() failed:", e)
+			log.Println("writeString() failed:", e)
 			return nil
 		}
 
@@ -84,17 +85,17 @@ var Bytes_` + name + " =[]byte{")
 func parseFile(fname string) {
 	f, e := os.Stat(fname)
 	if e != nil {
-		fmt.Println(`os.State error :`, e)
+		log.Println(`os.State error :`, e)
 		return
 	}
 	root, e := os.Getwd()
 	if e != nil {
-		fmt.Println(`getwd error :`, e)
+		log.Println(`getwd error :`, e)
 		return
 	}
 	fo, e := os.OpenFile(root+"/views/"+getFirstName(f.Name())+".go", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if e != nil {
-		fmt.Println(`openfile error :`, e)
+		log.Println(`openfile error :`, e)
 		return
 	}
 	defer fo.Close()
@@ -102,20 +103,20 @@ func parseFile(fname string) {
 
 var Str_` + getFirstName(f.Name()) + " =`")
 	if e != nil {
-		fmt.Println("writeString() failed:", e)
+		log.Println("writeString() failed:", e)
 		return
 	}
 	fi, e := os.OpenFile(fname, os.O_RDONLY, 0644)
 	if e != nil {
-		fmt.Println("fi() failed:", e)
+		log.Println("fi() failed:", e)
 		return
 	}
 	defer fi.Close()
 	_, e = io.Copy(fo, fi)
 	if e != nil {
-		fmt.Println("copy() failed:", e)
+		log.Println("copy() failed:", e)
 		return
 	}
 	fo.WriteString("`\n")
-	fmt.Println(root + "views/" + getFirstName(f.Name()) + ".go")
+	log.Println(root + "views/" + getFirstName(f.Name()) + ".go")
 }
